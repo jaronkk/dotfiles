db7ad654545eb50b2fc31b2241604953384e5d88
Jaron Kennel <jaronkk@gmail.com>
2015-06-18 11:39:16 -0400

    nvm path

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,3 +2,5 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="$PATH:/usr/local/opt/go/libexec/bin"
 export GOPATH="$HOME/workspace/go"
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
